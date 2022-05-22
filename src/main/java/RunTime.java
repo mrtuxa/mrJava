3faade312842dbab6015e3bd60516045b3df44b2
mrtuxa <tobias.daut1234@gmail.com>
2022-05-22 14:04:10 +0000

    Discord Rich Presence

--- src/main/java/RunTime.java
+++ src/main/java/RunTime.java
@@ -6,14 +6,14 @@ import org.javacord.api.DiscordApiBuilder;
 public class RunTime {
     public static void main(String[] args) {
         TokenClass secret = new TokenClass();
-
+        System.out.println("Discord API is starting...");
         DiscordApi api = new DiscordApiBuilder()
                 .setToken(secret.getToken())
                 .login()
                 .join();
 
-        api.updateActivity(">help for informations", "https://rr.noordstar.me/6480fa80");
-
+        api.updateActivity(">help for informations", "https://www.youtube.com/watch?v=xvFZjo5PgG0");
+        System.out.println("Discord API is running.");
 
     }
 }
