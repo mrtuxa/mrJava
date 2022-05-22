a592439040407f60d7cf8515ebd5d583ad8246c9
mrtuxa <tobias.daut1234@gmail.com>
2022-05-22 14:06:35 +0000

    Discord Rich Presence with config file

--- src/main/java/cfg/Settings.java
+++ src/main/java/cfg/Settings.java
@@ -1,4 +1,22 @@
 package cfg;
-
 public class Settings {
+    private String RPCname = ">help for informations";
+    private String RPCurl = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+
+    public String getRPCname() {
+        return RPCname;
+    }
+
+    public String getRPCurl() {
+        return RPCurl;
+    }
+
+    public void setRPCname(String RPCname) {
+        this.RPCname = RPCname;
+    }
+
+    public void setRPCurl(String RPCurl) {
+        this.RPCurl = RPCurl;
+    }
+
 }
