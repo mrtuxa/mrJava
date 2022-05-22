1c7b2ef938b1d06865231fcc2910517da94f9ee9
mrtuxa <tobias.daut1234@gmail.com>
2022-05-22 16:49:27 +0000

    Commit via Intellij IDEA

--- src/main/java/listeners/OnServerJoin.java
+++ src/main/java/listeners/OnServerJoin.java
@@ -1,4 +1,15 @@
 package listeners;
 
-public class OnServerJoin {
+import org.javacord.api.event.server.ServerJoinEvent;
+import org.javacord.api.listener.server.ServerJoinListener;
+
+public class OnServerJoin implements ServerJoinListener {
+
+    /**
+     * @param serverJoinEvent
+     */
+    @Override
+    public void onServerJoin(ServerJoinEvent se) {
+
+    }
 }
