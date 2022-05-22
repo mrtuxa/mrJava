0aee5ebb9e9b0ce76f1b3c786faf86d001295223
mrtuxa <tobias.daut1234@gmail.com>
2022-05-22 14:10:39 +0000

    Discord Rich Presence with config file

--- src/main/java/RunTime.java
+++ src/main/java/RunTime.java
@@ -1,3 +1,4 @@
+import cfg.Settings;
 import cfg.TokenClass;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
@@ -6,13 +7,14 @@ import org.javacord.api.DiscordApiBuilder;
 public class RunTime {
     public static void main(String[] args) {
         TokenClass secret = new TokenClass();
+        Settings s = new Settings();
         System.out.println("Discord API is starting...");
         DiscordApi api = new DiscordApiBuilder()
                 .setToken(secret.getToken())
                 .login()
                 .join();
 
-        api.updateActivity(">help for informations", "https://www.youtube.com/watch?v=xvFZjo5PgG0");
+        api.updateActivity(s.getRPCname(), s.getRPCurl());
         System.out.println("Discord API is running.");
 
     }
